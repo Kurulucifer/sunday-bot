96a6d72051c0e5a3b53520f1b035a6b666303639
KamilianovAA <74040345+KamilianovAA@users.noreply.github.com>
2024-12-05 12:41:25 +0400

    remade dove command to not stall, added separate folder for pasta

--- data.py
+++ data.py
@@ -1,8 +1,6 @@
 import os
 import json
 
-def grab_parent(file):
-    path = os.getcwd()
-    parent = os.path.dirname(path)
-    with open(parent + f'\{file}.json', 'r') as f:
+def grab_json(file):
+    with open(f'{file}.json', 'r') as f:
         return json.load(f)
